c805f319851a8705ed74545a1d5c33c4958f4399
Thibault Miranda de Oliveira <thibaultmiranda@epitech.eu>
2016-12-16 18:15:20 +0100

    add rebase

--- app.js
+++ app.js
@@ -17,11 +17,18 @@ app.post('/', jsonparse, function(req, res) {
     if (elem.name == body.project.name && elem.branch == body.ref.split('/')[2]){
       var git = require('simple-git')(elem.directory);
       git.fetch(function(err, data){
-        console.log(data);
+        console.log('fetch: ', data);
         if (err != null)
           fs.writeFile("/var/log/gitlab-autodeploy/gitlab-autodeploy.log", "[ERROR]: " + err);
         else
           fs.writeFile("/var/log/gitlab-autodeploy/gitlab-autodeploy.log", "[INFO]" + data);
+      });
+      git.rebase(['origin/' + elem.branch, elem.branch], function (err, data) {
+        console.log('rebase: ', data);
+        if (err != null)
+          fs.writeFile("/var/log/gitlab-autodeploy/gitlab-autodeploy.log", "[ERROR]: " + err);
+        else
+          fs.writeFile("/var/log/gitlab-autodeploy/gitlab-autodeploy.log", "[info]: " + data);
       });
     }
   });
