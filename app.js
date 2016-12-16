d4977cc64a2188b4eaa69faf9b5363991ce37ccf
Thibault Miranda de Oliveira <thibaultmiranda@epitech.eu>
2016-12-16 17:55:55 +0100

    test

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 var express = require('express');
-var body-parser = require('body-parser');
+var bodyparser = require('body-parser');
+var fs = require('fs');
 
-var jsonparse = body-parser.json();
+var config = require('./config.json').projects;
+
+var jsonparse = bodyparser.json();
 
 var app = express();
 
 app.post('/', jsonparse, function(req, res) {
-  res.sendStatus(200);
-  console.log(req.body);
+  res.sendStatus(200);  
+
+  var body = req.body;
+
+  config.forEach(function(elem, index) {
+    console.log(body.project.name.split('/')[2]);
+    if (elem.name == body.project.name && elem.branch == body.project.name.split('/')[2]){
+      var git = require('simple-git')(elem.directory);
+      git.fetch(function(err, data){
+        if (err == null)
+          fs.writeFile("/var/log/gitlab-autodeploy/gitlab-autodeploy.log", "[ERROR]: " + err);
+        else
+          fs.writeFile("/var/log/gitlab-autodeploy/gitlab-autodeploy.log", "[INFO]" + data);
+      });
+    }
+  });
 });
 
-app.listen(9090);
+app.listen(9123);
